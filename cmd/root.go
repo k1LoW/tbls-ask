a54d4486eb0951b00722e3c9b776a61573afce07
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-06-03 23:13:38 +0900

    enable gemini

--- cmd/root.go
+++ cmd/root.go
@@ -30,6 +30,7 @@ import (
 	"os"
 	"strings"
 
+	"github.com/k1LoW/tbls-ask/gemini"
 	"github.com/k1LoW/tbls-ask/openai"
 	"github.com/k1LoW/tbls-ask/version"
 	"github.com/k1LoW/tbls/datasource"
@@ -47,15 +48,30 @@ var (
 	labels   []string
 )
 
+// OpenAI または Gemini を型として持つ変数を作成する
+// この変数は、OpenAI または Gemini の Ask メソッドを呼び出すために使用される
+var m interface {
+	Ask(ctx context.Context, q string, s *schema.Schema) (string, error)
+	AskQuery(ctx context.Context, q string, s *schema.Schema) (string, error)
+}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:          "tbls-ask",
-	Short:        "ask OpenAI using the datasource",
-	Long:         `ask OpenAI using the datasource.`,
+	Short:        "ask LLM using the datasource",
+	Long:         `ask LLM using the datasource.`,
 	SilenceUsage: true,
 	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-		if os.Getenv("OPENAI_API_KEY") == "" {
-			return errors.New("OPENAI_API_KEY is not set")
+		if strings.HasPrefix(model, "gpt") {
+			if os.Getenv("OPENAI_API_KEY") == "" {
+				return errors.New("OPENAI_API_KEY is not set")
+			}
+		} else if strings.HasPrefix(model, "gemini") {
+			if os.Getenv("GEMINI_API_KEY") == "" {
+				return errors.New("GEMINI_API_KEY is not set")
+			}
+		} else {
+			return errors.New("model is not supported")
 		}
 		if os.Getenv("TBLS_SCHEMA") == "" {
 			return errors.New("TBLS_SCHEMA is not set")
@@ -64,7 +80,12 @@ var rootCmd = &cobra.Command{
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.Background()
-		o := openai.New(os.Getenv("OPENAI_API_KEY"), model)
+		// model が gpt から始まる場合は OpenAI を使う
+		if strings.HasPrefix(model, "gpt") {
+			m = openai.New(os.Getenv("OPENAI_API_KEY"), model)
+		} else if strings.HasPrefix(model, "gemini") {
+			m = gemini.New(os.Getenv("GEMINI_API_KEY"), model)
+		}
 		q := strings.Join(args, " ")
 		s, err := datasource.AnalyzeJSONStringOrFile(os.Getenv("TBLS_SCHEMA"))
 		if err != nil {
@@ -81,12 +102,12 @@ var rootCmd = &cobra.Command{
 
 		var a string
 		if query {
-			a, err = o.AskQuery(ctx, q, s)
+			a, err = m.AskQuery(ctx, q, s)
 			if err != nil {
 				return err
 			}
 		} else {
-			a, err = o.Ask(ctx, q, s)
+			a, err = m.Ask(ctx, q, s)
 			if err != nil {
 				return err
 			}
--- gemini/gemini.go
+++ gemini/gemini.go
@@ -5,39 +5,53 @@ import (
 	"context"
 	"fmt"
 	"html/template"
-	"log"
 	"os"
 
 	"github.com/google/generative-ai-go/genai"
-	"github.com/k1LoW/tbls/config"
-	"github.com/k1LoW/tbls/datasource"
 	"github.com/k1LoW/tbls-ask/templates"
+	"github.com/k1LoW/tbls/schema"
 	"google.golang.org/api/option"
 )
 
-func Ask(query string, path string) string {
-	if os.Getenv("GEMINI_API_KEY") == "" {
-		return "GEMINI_API_KEY is not set"
+const (
+	DefaultModelChat = "gemini-pro"
+	quoteStart       = "```sql"
+	quoteEnd         = "```"
+)
+
+type Gemini struct {
+	client          *genai.Client
+	model           string
+	promptTmpl      string
+	queryPromptTmpl string
+}
+
+func New(key, model string) *Gemini {
+	client, err := genai.NewClient(context.Background(), option.WithAPIKey(key))
+	if err != nil {
+		return nil
 	}
-	ctx := context.Background()
+	return &Gemini{
+		client:          client,
+		model:           model,
+		promptTmpl:      templates.DefaultPromtTmpl,
+		queryPromptTmpl: templates.DefaultQueryPromptTmpl,
+	}
+}
+
+func (g *Gemini) Ask(ctx context.Context, q string, s *schema.Schema) (string, error) {
+	if os.Getenv("GEMINI_API_KEY") == "" {
+		return "", fmt.Errorf("GEMINI_API_KEY is not set")
+	}
 	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
 	if err != nil {
-		log.Printf("Failed to create genai client: %v", err)
-		return "Failed to create genai client"
+		return "", err
 	}
 	defer client.Close()
 
-	dsn := config.DSN{URL: path}
-	s, err := datasource.Analyze(dsn)
-	if err != nil {
-		log.Printf("Failed to analyze schema: %v", err)
-		return "Failed to analyze schema"
-	}
-
 	tpl, err := template.New("").Parse(templates.DefaultPromtTmpl)
 	if err != nil {
-		log.Printf("Failed to parse template: %v", err)
-		return "Failed to parse template"
+		return "", err
 	}
 	buf := new(bytes.Buffer)
 	if err := tpl.Execute(buf, map[string]any{
@@ -45,20 +59,52 @@ func Ask(query string, path string) string {
 		"QuoteStart":      "```sql",
 		"QuoteEnd":        "```",
 		"DDL":             templates.GenerateDDLRoughly(s),
-		// "Question":        query,
+		"Question":        q,
 	}); err != nil {
-		log.Printf("Failed to execute template: %v", err)
-		return "Failed to execute template"
+		return "", err
 	}
 
-	model := client.GenerativeModel("gemini-pro")
+	model := client.GenerativeModel(g.model)
 	resp, err := model.GenerateContent(ctx, genai.Text(buf.String()))
 	if err != nil {
-		log.Printf("Failed to generate content: %v", err)
-		return "Failed to generate content"
+		return "", err
 	}
 	answer := extractResponse(resp)
-	return answer
+	return answer, nil
+}
+
+func (g *Gemini) AskQuery(ctx context.Context, q string, s *schema.Schema) (string, error) {
+	if os.Getenv("GEMINI_API_KEY") == "" {
+		return "", fmt.Errorf("GEMINI_API_KEY is not set")
+	}
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	tpl, err := template.New("").Parse(templates.DefaultQueryPromptTmpl)
+	if err != nil {
+		return "", err
+	}
+	buf := new(bytes.Buffer)
+	if err := tpl.Execute(buf, map[string]any{
+		"DatabaseVersion": templates.DatabaseVersion(s),
+		"QuoteStart":      "```sql",
+		"QuoteEnd":        "```",
+		"DDL":             templates.GenerateDDLRoughly(s),
+		"Question":        q,
+	}); err != nil {
+		return "", err
+	}
+
+	model := client.GenerativeModel(g.model)
+	resp, err := model.GenerateContent(ctx, genai.Text(buf.String()))
+	if err != nil {
+		return "", err
+	}
+	answer := extractResponse(resp)
+	return answer, nil
 }
 
 func extractResponse(resp *genai.GenerateContentResponse) string {
