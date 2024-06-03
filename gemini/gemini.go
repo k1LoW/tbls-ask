eb3dfe5d5f7d05af2d5bfd9fe2c7f920495af982
Hiroyuki Kuromiya <15026387+kromiii@users.noreply.github.com>
2024-06-03 21:09:18 +0900

    add gemini.go

---
+++ gemini/gemini.go
@@ -0,0 +1,87 @@
+package gemini
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"html/template"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/k1LoW/tbls/config"
+	"github.com/k1LoW/tbls/datasource"
+	"google.golang.org/api/option"
+)
+
+func Ask(query string, path string) string {
+	if os.Getenv("GEMINI_API_KEY") == "" {
+		return "GEMINI_API_KEY is not set"
+	}
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	if err != nil {
+		log.Printf("Failed to create genai client: %v", err)
+		return "Failed to create genai client"
+	}
+	defer client.Close()
+
+	dsn := config.DSN{URL: path}
+	s, err := datasource.Analyze(dsn)
+	if err != nil {
+		log.Printf("Failed to analyze schema: %v", err)
+		return "Failed to analyze schema"
+	}
+
+	tpl, err := template.New("").Parse(defaultPromtTmpl)
+	if err != nil {
+		log.Printf("Failed to parse template: %v", err)
+		return "Failed to parse template"
+	}
+	buf := new(bytes.Buffer)
+	if err := tpl.Execute(buf, map[string]any{
+		"DatabaseVersion": databaseVersion(s),
+		"QuoteStart":      "```sql",
+		"QuoteEnd":        "```",
+		"DDL":             generateDDLRoughly(s),
+		// "Question":        query,
+	}); err != nil {
+		log.Printf("Failed to execute template: %v", err)
+		return "Failed to execute template"
+	}
+
+	model := client.GenerativeModel("gemini-pro")
+	cs := model.StartChat()
+	cs.History = []*genai.Content{
+		{
+			Role:  "user",
+			Parts: []genai.Part{genai.Text(buf.String())},
+		},
+		{
+			Role:  "model",
+			Parts: []genai.Part{genai.Text("質問を入力してください")},
+		},
+	}
+	resp, err := cs.SendMessage(ctx, genai.Text(query))
+	// resp, err := model.GenerateContent(ctx, genai.Text(buf.String()))
+	if err != nil {
+		log.Printf("Failed to generate content: %v", err)
+		return "Failed to generate content"
+	}
+	answer := extractResponse(resp)
+	return answer
+}
+
+func extractResponse(resp *genai.GenerateContentResponse) string {
+	response := ""
+	for _, candidate := range resp.Candidates {
+		if candidate.Content != nil && len(candidate.Content.Parts) > 0 {
+			for _, part := range candidate.Content.Parts {
+				if part != nil {
+					response = fmt.Sprintf("%s", part)
+				}
+			}
+		}
+	}
+	return response
+}
